75503bd6c3a6a0bda273723f973b3b9caaf24bb0
lenovo <lenovo@101.0.15.73>
2020-04-26 20:56:39 +0530

    Open google in Browser

--- AnchalKohli/src/OpenBrowser.java
+++ AnchalKohli/src/OpenBrowser.java
@@ -6,7 +6,6 @@ public class OpenBrowser {
 	{
 		System.setProperty("webdriver.gecko.driver",".\\Drivers\\geckodriver.exe");
 		WebDriver driver =new FirefoxDriver();
-		//driver.manage().window().maximize();
 		Thread.sleep(3000);
 		driver.get("https://www.google.com/?gws_rd=ssl");
 		Thread.sleep(3000);
