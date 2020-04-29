9f98c89320b15b8f55c89c6dc712d378a3e05c64
lenovo <lenovo@lenovo-PC>
2020-04-29 19:59:47 +0530

    xpath trials

--- AnchalKohli/src/TestClass.java
+++ AnchalKohli/src/TestClass.java
@@ -1,8 +1,27 @@
 import java.lang.String;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 public class TestClass 
 {
-	public static void main(String args[])
+	public static void main(String args[]) throws InterruptedException
 	{
-	System.out.println("Bye");
+	System.setProperty("webdriver.gecko.driver", ".\\Drivers\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.get("https://demo.actitime.com/login.do");
+	Thread.sleep(3000);
+	
+	//ways to click on clock image
+	//WebElement cl=driver.findElement(By.xpath("//td[1][@id='logoContainer']/div/div/img"));
+	
+	
+	//ways to click on login button
+	//WebElement lgn=driver.findElement(By.xpath("//div[text()='Login ']"));
+	//WebElement lgn=driver.findElement(By.xpath("//a[@id='loginButton']/div"));
+	WebElement lgn=driver.findElement(By.xpath("//a[@id='loginButton']/div[text()='Login ']"));
+	lgn.click();
+	driver.close();
 	}
 }
